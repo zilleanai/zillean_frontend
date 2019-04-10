ab4a81af0d421a59f1805046e520edf18c2c46c7
Christian M <chriamue@gmail.com>
2019-04-10 15:22:37 +0200

    set host for frontend proxy

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -25,6 +25,10 @@ const backendHost = process.env.API_HOST || frontendHost
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 app.use(/^\/api|auth\//, proxy(`http://${backendHost}:${backendPort}`, {
+    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+      proxyReqOpts.headers['HOST'] = srcReq.headers['host'];
+      return proxyReqOpts;
+    },
     proxyReqPathResolver: (req) => req.baseUrl + req.url,
     limit: "1024mb",
 }))
